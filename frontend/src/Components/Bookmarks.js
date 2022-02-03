3776c102f1454e79b54aed84c796199506693628
Greg Testo <gregt@pursuit.org>
2022-02-03 12:15:43 -0400

    added get all bookmarks

--- frontend/src/Components/Bookmarks.js
+++ frontend/src/Components/Bookmarks.js
@@ -6,7 +6,13 @@ const API = process.env.REACT_APP_API_URL;
 
 function Bookmarks() {
   const [bookmarks, setBookmarks] = useState([]);
-  useEffect(() => {}, []);
+  
+  useEffect(() => {
+    axios.get( API + "/bookmarks" ).then((res)=>{
+      setBookmarks(res.data);
+    })
+  }, []);
+
   return (
     <div className="Bookmarks">
       <section>
