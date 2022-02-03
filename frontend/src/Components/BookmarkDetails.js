d431568980883fb0c9ca365f6cf599c772a57c06
Greg Testo <gregt@pursuit.org>
2022-02-03 11:46:54 -0400

    added frontend

---
+++ frontend/src/Components/BookmarkDetails.js
@@ -0,0 +1,8 @@
+// import { useState } from "react";
+
+function BookmarkDetails() {
+  // const [bookmark, setBookmark] = useState([]);
+  return <article></article>;
+}
+
+export default BookmarkDetails;
---
+++ frontend/src/Components/Bookmarks.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Bookmark from "./Bookmark";
+
+const API = process.env.REACT_APP_API_URL;
+
+function Bookmarks() {
+  const [bookmarks, setBookmarks] = useState([]);
+  useEffect(() => {}, []);
+  return (
+    <div className="Bookmarks">
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Take me there</th>
+              <th>See this bookmark</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookmarks.map((bookmark) => {
+              return <Bookmark key={bookmark.id} bookmark={bookmark} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export default Bookmarks;
